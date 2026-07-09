bec975aae48c44a7cdca11b893d66311c85ceebf
HelloAdrian2k <mz.adriaan@gmail.com>
2026-07-09 23:08:38 +0200

    feat: add task manager menu

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ main.py is the core file of the Tasks CLI
 It defines the TaskManager class and the Task class
 """
 
+import subprocess
+
 class Task:
     def __init__(self, task_id, name):
         self.task_id = task_id
@@ -12,15 +14,15 @@ class Task:
 class TaskManager:
     def __init__(self):
         self.task_list = []
+        self.show_menu()
 
     def create(self,name):
-        task_id = self.task_list[-1].task_id+1 if self.task_list else 1
-        self.task_list.append(Task(task_id,name))
+        self.task_list.append(Task(self.get_next_task_id(),name))
 
     def show_tasks(self):
         for i in self.task_list:
             task_status = "[X]" if i.completed else "[]"
-            print(f"--- TASK {i.task_id}. {i.name} {task_status}")
+            print(f"{i.task_id}. {i.name} {task_status}")
 
     def find(self, task_id):
         for i in self.task_list:
@@ -36,7 +38,40 @@ class TaskManager:
         task = self.find(task_id)
         if task: self.task_list.remove(task)
 
+    def get_next_task_id(self):
+        return self.task_list[-1].task_id+1 if self.task_list else 1
+
+    def clear_console(self):
+        subprocess.call('cls', shell=True)
+
+    def show_menu(self):
+        show_menu = True
+        while show_menu:
+            self.clear_console()
+            last_task_id = self.get_next_task_id()
+            print(f"===== Task CLI =====")
+            self.show_tasks()
+            print("\n---")
+            print(f"{last_task_id}. Create a new task")
+            print(f"0. Exit")
+
+            # Check value error
+            try:
+                opt = int(input('\nSelect ---> '))
+            except ValueError:
+                continue
+
+            # Check if selected option is valid
+            if opt < 0 or opt > last_task_id:
+                continue
+            self.clear_console()
+            if opt == 0:
+                exit()
+            elif opt == last_task_id:
+                new_task = input("New task: ")
+                if new_task:
+                    self.create(new_task)
+
 task_manager = TaskManager()
 
-task_manager.create("Comprar leche")
 
