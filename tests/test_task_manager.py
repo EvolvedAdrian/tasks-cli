f2b7fda4793317ba5aea75e11eb6a8c3cfd2f78d
HelloAdrian2k <mz.adriaan@gmail.com>
2026-07-10 17:29:45 +0200

    test: add some edge cases and persistence tests

--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -3,6 +3,7 @@ Tests file : Arrange --> Act --> Assert
 """
 
 import unittest
+import json
 
 from src.task_manager import TaskManager
 
@@ -18,6 +19,20 @@ class TestTaskManager(unittest.TestCase):
         self.manager = TaskManager(TEST_FILE)
         self.manager.create_task("Task")
     
+    def get_first_task(self):
+        with open(TEST_FILE, "r") as task_file:
+            try:
+                stored_tasks = json.load(task_file)[0]
+            except IndexError:
+                stored_tasks = {}
+        
+        try:
+            memory_tasks = self.manager.task_list[0].__dict__
+        except IndexError:
+            memory_tasks = {}
+
+        return (stored_tasks,memory_tasks)
+
     def test_create_task(self):
         self.assertEqual(len(self.manager.task_list), 1)
 
@@ -26,6 +41,18 @@ class TestTaskManager(unittest.TestCase):
         next_task_id = self.manager.get_next_task_id()
 
         self.assertEqual(next_task_id, 2)
+
+    def test_autoincremental_task_id(self):
+        self.assertEqual(self.manager.task_list[-1].task_id, 1)
+
+        self.manager.create_task("New task")
+
+        self.assertEqual(self.manager.task_list[-1].task_id, 2)
+
+        self.manager.create_task("Last task")
+
+        self.assertEqual(self.manager.task_list[-1].task_id, 3)
+
     
     def test_toggle_completed(self):
         self.assertFalse(self.manager.task_list[0].completed)
@@ -54,14 +81,41 @@ class TestTaskManager(unittest.TestCase):
 
     def test_find_non_existing_task(self):
         # Act --> Check non existing task was not found
-        unfounded_task_0 = self.manager.find_task(0)
-        unfounded_task_2 = self.manager.find_task(2)
+        self.assertIsNone(self.manager.find_task(999))
 
-        self.assertIsNone(unfounded_task_0)
-        self.assertIsNone(unfounded_task_2)
+    def test_delete_impossible_task(self):
+        # Act --> Delete non existing task
+        self.manager.delete_task(999)
 
+        self.assertEqual(len(self.manager.task_list), 1)
+    
+    def test_toggle_impossible_task(self):
+        # Act --> Toggle non existing task
+        self.manager.toggle_completed(999)
 
+        self.assertFalse(self.manager.task_list[0].completed)
 
+    def test_task_persistence_after_creation(self):
+        # Act --> Action to check persistence on task creation
+        # first_task[0] = STORED ON JSON
+        # first task[1] = MEMORY TASK (NOT PERSISTED)
+        first_task = self.get_first_task()
 
+        self.assertDictEqual(first_task[0], first_task[1])
 
+    def test_task_persistence_after_toggle_completion(self):
+        # Act --> Action to check persistence on toggle completed
+        self.manager.toggle_completed(1)
 
+        first_task = self.get_first_task()
+
+        self.assertDictEqual(first_task[0], first_task[1])
+
+    def test_task_removed_after_deletion(self):
+        # Act --> Action to check persistence on task deletion
+        self.manager.delete_task(1)
+
+        first_task = self.get_first_task()
+
+        self.assertDictEqual(first_task[0], first_task[1])
+    
